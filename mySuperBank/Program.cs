80ec041bdf9054a97d61a7713a8a409c5291eef3
Talos-1 <42595602+Talos-1@users.noreply.github.com>
2022-08-29 17:08:05 +1000

    mySuperBank exercises added

---
+++ mySuperBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace mySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            // Test for a negative balance
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch(InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw.");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            // Test that the initial balances must be positive
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+        }
+    }
+}
